0780c0d33365f9752c002a0fb156f720972e9041
calebcouperus <124112022+calebcouperus@users.noreply.github.com>
2023-03-14 10:12:39 +1300

    Changes

---
+++ Movie_Tickets_v1.py
@@ -0,0 +1,20 @@
+"""Movie theatre ticketing system - v1
+Welcome screen and variables
+Created by Caleb Couperus
+"""
+
+
+# Component 1 - welcome screen and setup variables
+def sell_ticket():
+    print('********** Fanfare Movies - ticket system **********\n')
+
+    adult_tickets = 0
+    student_tickets = 0
+    child_tickets = 0
+    gift_tickets = 0
+    tickets_sold = 0
+    total_sales = 0
+
+
+# Main routine
+sell_ticket()
